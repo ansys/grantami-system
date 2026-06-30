ab4b14af8a3809db32564109a2630f3408ae3296
Andy-Grigg <Andy-Grigg@users.noreply.github.com>
2026-06-30 11:36:36 +0000

    DOC: update development documentation 78171344273e60387c7f9d223ef2122c924ad12e

---
+++ version/1.0/_downloads/3d86a569e73a6530fc3656cc6f98b872/2-3_Anonymize_activity_report.py
@@ -0,0 +1,64 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.17.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# # Anonymize an activity report
+
+# This example shows how to anonymize an activity report.
+
+# ## Fetch the activity report
+
+# Import the ``Connection`` class, create the connection, and fetch the activity report. For more
+# information, see the [Basic Usage](../1_Basic_usage.ipynb) and
+# [Access an activity report](2-1_Access_activity_report.ipynb) examples.
+
+# +
+from ansys.grantami.system import Connection
+
+connection = Connection("http://my_grantami_server/mi_servicelayer").with_autologon()
+client = connection.connect()
+items = client.get_activity_report()
+# -
+
+# ## Anonymize the activity report
+
+# `ActivityItem` objects are immutable, and so the `username` for an `ActivityItem` cannot be changed directly.
+# However, the `ActivityItem` class is a dataclass, so we can use the `dataclasses.replace()` function to create a new
+# object with the required modification.
+
+# +
+from dataclasses import replace
+
+anonymized_usernames = {}
+anonymized_items = []
+
+for item in items:
+    current_username = item.username
+
+    try:
+        # If an anonymized username has already been created, retrieve it
+        anonymized_name = anonymized_usernames[current_username]
+    except KeyError:
+        # If not,
+        anonymized_name = f"Anonymous user {len(anonymized_items)}"
+        anonymized_usernames[current_username] = anonymized_name
+
+    anonymized_item = replace(item, username=anonymized_name)
+    anonymized_items.append(anonymized_item)
+# -
+
+# Each item in the activity report now contains an anonymized username. Any analytics that aggregate unique numbers of
+# users will produce the same results, but usernames of individuals will not be included in any more granular results.
+
+print(anonymized_items[0])
